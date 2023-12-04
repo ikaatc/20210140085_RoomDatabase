096f51b3a122bc8d7496425580e673ee3cb7212b
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-04 22:45:18 +0700

    membuat class HomeViewModel

--- app/src/main/java/com/example/pertsepuluhdatasiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/example/pertsepuluhdatasiswa/model/HomeViewModel.kt
@@ -1,2 +1,8 @@
 package com.example.pertsepuluhdatasiswa.model
 
+import androidx.lifecycle.ViewModel
+import com.example.pertsepuluhdatasiswa.Repositori.RepositoriSiswa
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+
+}
