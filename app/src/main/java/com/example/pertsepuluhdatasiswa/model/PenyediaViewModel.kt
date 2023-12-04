775a52df5c53bd78dd05f58852dfab2dcaa48d7f
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-04 23:56:47 +0700

    membuat ViewModelFactory untuk HomeViewModel dan EntryViewModel dengan mengakses repositori dari AplikasiSiswa.

--- app/src/main/java/com/example/pertsepuluhdatasiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertsepuluhdatasiswa/model/PenyediaViewModel.kt
@@ -1,2 +1,26 @@
 package com.example.pertsepuluhdatasiswa.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pertsepuluhdatasiswa.AplikasiSiswa
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+/**
+ * Fungsi ekstensi query untuk object [Application] dan mengembalikan sebuah instance dari [AplikasiSiswa]
+ */
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
