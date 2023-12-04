8f91f4a2d4ea2dbf648aee47e69b7d95a949eeda
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-04 23:31:55 +0700

    membuat batas waktu dalam milidetik (5000 ms) untuk operasi dalam companion object.

--- app/src/main/java/com/example/pertsepuluhdatasiswa/model/HomeViewModel.kt
+++ app/src/main/java/com/example/pertsepuluhdatasiswa/model/HomeViewModel.kt
@@ -4,5 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.example.pertsepuluhdatasiswa.Repositori.RepositoriSiswa
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
-
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }
