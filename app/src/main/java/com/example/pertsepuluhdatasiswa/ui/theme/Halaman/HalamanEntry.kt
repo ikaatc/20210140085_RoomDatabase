023c8420716c2740c4667521b8f531317591e1da
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-05 12:31:42 +0700

    membuat function EntrySiswaScreen untuk bagian utama dari layar entry siswa

--- app/src/main/java/com/example/pertsepuluhdatasiswa/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/pertsepuluhdatasiswa/ui/theme/Halaman/HalamanEntry.kt
@@ -1,9 +1,21 @@
 package com.example.pertsepuluhdatasiswa.ui.theme.Halaman
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertsepuluhdatasiswa.R
+import com.example.pertsepuluhdatasiswa.model.EntryViewModel
+import com.example.pertsepuluhdatasiswa.model.PenyediaViewModel
 import com.example.pertsepuluhdatasiswa.navigasi.DestinasiNavigasi
 
 object DestinasiEntry: DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = R.string.entry_siswa
 }
+
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {}
