af471ec5fbd3c01aa6ddf3b1214ed892b194084d
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-05 17:48:47 +0700

    membuat function DataSiswa dan membuat Card

--- app/src/main/java/com/example/pertsepuluhdatasiswa/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/pertsepuluhdatasiswa/ui/theme/Halaman/HalamanHome.kt
@@ -1,9 +1,12 @@
 package com.example.pertsepuluhdatasiswa.ui.theme.Halaman
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -16,6 +19,7 @@ import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertsepuluhdatasiswa.R
 import com.example.pertsepuluhdatasiswa.data.Siswa
@@ -80,6 +84,18 @@ fun ListSiswa(
 }
 
 @Composable
-fun DataSiswa(siswa: Siswa, modifier: Modifier) {
+fun DataSiswa(
+    siswa: Siswa,
+    modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ) {
 
+        }
+    }
 }
