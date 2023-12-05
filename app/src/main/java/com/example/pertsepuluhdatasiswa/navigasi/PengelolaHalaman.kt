e91fb2f0433f62ce3ca9763bfba6d4d209e87b63
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-05 18:11:43 +0700

    menambahkan function SiswaTopAppBar

--- app/src/main/java/com/example/pertsepuluhdatasiswa/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertsepuluhdatasiswa/navigasi/PengelolaHalaman.kt
@@ -1,6 +1,9 @@
 package com.example.pertsepuluhdatasiswa.navigasi
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
@@ -8,6 +11,16 @@ import androidx.navigation.compose.rememberNavController
 fun SiswaApp(navController: NavHostController = rememberNavController()){
     HostNavigasi(navController = navController)
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {}
 
 @Composable
 fun HostNavigasi(navController: NavHostController) {
