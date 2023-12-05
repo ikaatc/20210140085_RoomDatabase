87e468718f4d660cad5a6354bfe487f4045cc4f5
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-05 09:37:12 +0700

    membuat objek DestinasiEntry yang mengimplementasikan DestinasiNavigasi untuk rute dan judul halaman

---
+++ app/src/main/java/com/example/pertsepuluhdatasiswa/ui/theme/Halaman/HalamanEntry.kt
@@ -0,0 +1,9 @@
+package com.example.pertsepuluhdatasiswa.ui.theme.Halaman
+
+import com.example.pertsepuluhdatasiswa.R
+import com.example.pertsepuluhdatasiswa.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
