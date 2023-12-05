4cc1ca736e9611fa0d8f2f56214336295eef0e19
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-05 18:13:56 +0700

    mengedit/custom TopAppBar

--- app/src/main/java/com/example/pertsepuluhdatasiswa/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertsepuluhdatasiswa/navigasi/PengelolaHalaman.kt
@@ -1,11 +1,19 @@
 package com.example.pertsepuluhdatasiswa.navigasi
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.pertsepuluhdatasiswa.R
 
 @Composable
 fun SiswaApp(navController: NavHostController = rememberNavController()){
@@ -20,7 +28,23 @@ fun SiswaTopAppBar(
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {}
-) {}
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(id = R.string.back)
+                    )
+                }
+            }
+        }
+    )
+}
 
 @Composable
 fun HostNavigasi(navController: NavHostController) {
