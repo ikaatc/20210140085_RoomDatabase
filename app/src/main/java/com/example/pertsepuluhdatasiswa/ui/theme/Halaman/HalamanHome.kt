4581221ba42d0c12b780ee1febb33cc6738e392c
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-05 17:18:07 +0700

    menambahkan val scrollBehavior

--- app/src/main/java/com/example/pertsepuluhdatasiswa/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/pertsepuluhdatasiswa/ui/theme/Halaman/HalamanHome.kt
@@ -1,7 +1,11 @@
 package com.example.pertsepuluhdatasiswa.ui.theme.Halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertsepuluhdatasiswa.R
 import com.example.pertsepuluhdatasiswa.model.HomeViewModel
@@ -13,9 +17,12 @@ object DestinasiHome: DestinasiNavigasi {
     override val titleRes = R.string.app_name
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     navigateToItemEntry: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
-) {}
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+}
