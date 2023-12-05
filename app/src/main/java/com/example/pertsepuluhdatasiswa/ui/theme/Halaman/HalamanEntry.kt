334016dc7e858448ed43fa1702112154ffec1064
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-05 15:45:30 +0700

    membuat function EntrySiswaBody dan menambahkan komponen entri data siswa

--- app/src/main/java/com/example/pertsepuluhdatasiswa/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/pertsepuluhdatasiswa/ui/theme/Halaman/HalamanEntry.kt
@@ -6,10 +6,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertsepuluhdatasiswa.R
+import com.example.pertsepuluhdatasiswa.model.DetailSiswa
 import com.example.pertsepuluhdatasiswa.model.EntryViewModel
 import com.example.pertsepuluhdatasiswa.model.PenyediaViewModel
+import com.example.pertsepuluhdatasiswa.model.UIStateSiswa
 import com.example.pertsepuluhdatasiswa.navigasi.DestinasiNavigasi
 
 object DestinasiEntry: DestinasiNavigasi {
@@ -27,3 +30,11 @@ fun EntrySiswaScreen(
     val coroutineScope = rememberCoroutineScope()
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 }
+
+@Composable
+fun EntrySiswaBody(
+    uiStateSiswa: UIStateSiswa,
+    onSiswaValueChange: (DetailSiswa) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {}
