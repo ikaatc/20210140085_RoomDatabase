0a38ddec43186d7c9f1002ca910f282384bccad9
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-05 17:05:34 +0700

    membuat DestinasiHome untuk mengimplementasikan DestinasiNavigasi untuk route dan title

---
+++ app/src/main/java/com/example/pertsepuluhdatasiswa/ui/theme/Halaman/HalamanHome.kt
@@ -0,0 +1,9 @@
+package com.example.pertsepuluhdatasiswa.ui.theme.Halaman
+
+import com.example.pertsepuluhdatasiswa.R
+import com.example.pertsepuluhdatasiswa.navigasi.DestinasiNavigasi
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
