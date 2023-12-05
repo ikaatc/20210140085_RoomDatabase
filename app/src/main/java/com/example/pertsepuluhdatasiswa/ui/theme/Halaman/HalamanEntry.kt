eab8949e71e14c3d8d589d6317fd2deed99d0acc
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-05 12:35:30 +0700

    inisialisasi coroutineScope dan enterAlwaysScrollBehavior

--- app/src/main/java/com/example/pertsepuluhdatasiswa/ui/theme/Halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/pertsepuluhdatasiswa/ui/theme/Halaman/HalamanEntry.kt
@@ -1,6 +1,10 @@
 package com.example.pertsepuluhdatasiswa.ui.theme.Halaman
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertsepuluhdatasiswa.R
@@ -13,9 +17,13 @@ object DestinasiEntry: DestinasiNavigasi {
     override val titleRes = R.string.entry_siswa
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EntrySiswaScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
-) {}
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+}
