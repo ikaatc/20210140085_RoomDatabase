305d59cf2a472113753740572a0882c139cc0f81
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-05 18:31:31 +0700

    mengubah compileSDK menjadi 34 dan mengubah string

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.pertsepuluhdatasiswa"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.pertsepuluhdatasiswa"
