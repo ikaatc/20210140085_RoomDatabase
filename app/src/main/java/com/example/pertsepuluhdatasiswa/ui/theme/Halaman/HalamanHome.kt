1394d44faf54473fca60e38a78e3d0d9382a1b4f
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-05 17:50:52 +0700

    menambahkan code untuk meanampilkan nama siswa dan ikon telepon

--- app/src/main/java/com/example/pertsepuluhdatasiswa/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/pertsepuluhdatasiswa/ui/theme/Halaman/HalamanHome.kt
@@ -2,12 +2,18 @@ package com.example.pertsepuluhdatasiswa.ui.theme.Halaman
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -95,7 +101,25 @@ fun DataSiswa(
             modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
             verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
         ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = siswa.nama,
+                    style = MaterialTheme.typography.titleLarge
+                )
 
+                Spacer(Modifier.weight(1f))
+
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = null,
+                )
+                Text(
+                    text = siswa.telpon,
+                    style = MaterialTheme.typography.titleMedium,
+                )
+            }
         }
     }
 }
