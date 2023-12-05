348f49a7297f8bd1c5f4f34645dc92b3d74f3d81
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-12-05 17:09:41 +0700

    membuat function HomeScreen

--- app/src/main/java/com/example/pertsepuluhdatasiswa/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/pertsepuluhdatasiswa/ui/theme/Halaman/HalamanHome.kt
@@ -1,9 +1,21 @@
 package com.example.pertsepuluhdatasiswa.ui.theme.Halaman
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertsepuluhdatasiswa.R
+import com.example.pertsepuluhdatasiswa.model.HomeViewModel
+import com.example.pertsepuluhdatasiswa.model.PenyediaViewModel
 import com.example.pertsepuluhdatasiswa.navigasi.DestinasiNavigasi
 
 object DestinasiHome: DestinasiNavigasi {
     override val route = "home"
     override val titleRes = R.string.app_name
 }
+
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {}
